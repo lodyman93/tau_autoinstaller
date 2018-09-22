6168dfc913f8978793b56f993cd0f5a56e303a70
lodyman93 <lodywork@gmail.com>
2018-09-22 02:03:31 +0200

    Update tau.sh

--- tau.sh
+++ tau.sh
@@ -2,7 +2,6 @@
 #Bash script to automatically download all Taucoin dependancies ,build it and install it
 #Should work on any debian based distro and fully tested on ubuntu 12.xx-14.xx-16.xx
 #by : lod
-
 echo " _        _______  ______  "
 echo "( \      (  ___  )(  __  \ "
 echo "| (      | (   ) || (  \  )"
@@ -11,43 +10,32 @@ echo "| |      | |   | || |   | |"
 echo "| |      | |   | || |   ) |"
 echo "| (____/\| (___) || (__/  )"
 echo "(_______/(_______)(______/ "
-echo
+echo ""
 echo "Tau Automaic installer By: LOD                       "
-
-
 read -p "Do you wish to download dependencies, build and install Taucoin-QT wallet? " -n 1 -r
-echo    
+echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
- echo "this will take some time depending on your CPU/RAM/Internet speed"
- echo "please be patient !! :) "
-
-  echo "adding bitcoin repo for levelDB "
+echo "this will take some time depending on your CPU/RAM/Internet speed"
+echo "please be patient !! :) "
+echo "adding bitcoin repo for levelDB "
 sudo add-apt-repository ppa:bitcoin/bitcoin -y;
-  echo "updating all repos "
-
+echo "updating all repos "
 sudo apt-get update;
 echo "installing dependancies "
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libqt4-dev libprotobuf-dev protobuf-compiler libdb4.8-dev libdb4.8++-dev -y;
-
-
-
 mkdir tau;
 cd tau ;
 echo "cloning tau repo from GitHub "
 git clone https://github.com/Tau-Coin/taucoin.git ;
-
 cd taucoin;
-
 echo "building Tau "
 ./autogen.sh ;
-
- echo "now the slow part of compiling "
+echo "now the slow part of compiling "
 make ;
 echo "almost done !!"
 make install ;
- echo "All done , thanks for being patient !! ;) "
- 
- echo "Now you can start the deamon with /" taucoind /" to work with CLI"
+echo "All done , thanks for being patient !! ;) "
+echo "Now you can start the deamon with /" taucoind /" to work with CLI"
 echo "or Start QT with /"taucoin-qt/" wallet if you have a desktop environment"
 fi
