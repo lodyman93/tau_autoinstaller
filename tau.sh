67d8d9d6a857ddbd255bec54b48942781a5fd8b5
lodyman93 <lodywork@gmail.com>
2018-09-22 02:29:06 +0200

    Update tau.sh

--- tau.sh
+++ tau.sh
@@ -18,19 +18,20 @@ echo "adding bitcoin repo for levelDB "
 sudo add-apt-repository ppa:bitcoin/bitcoin -y;
 echo "updating all repos "
 sudo apt-get update;
-echo "installing dependancies "
+echo "installing dependancies..."
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libqt4-dev libprotobuf-dev protobuf-compiler libdb4.8-dev libdb4.8++-dev -y;
 mkdir tau;
 cd tau ;
-echo "cloning tau repo from GitHub "
-git clone https://github.com/Tau-Coin/taucoin.git ;
+echo "cloning tau repo from GitHub..."
+git clone https://github.com/Tau-Coin/taucoin.git;
 cd taucoin;
-echo "building Tau "
-./autogen.sh ;
-echo "now the slow part of compiling "
-make ;
+echo "building Tau..."
+./autogen.sh;
+./configure;
+echo "now the slow part of compiling...................."
+make;
 echo "almost done !!"
-make install ;
+make install;
 echo "All done , thanks for being patient !! "
-echo "Now you can start the deamon with /" taucoind /" to work with CLI"
-echo "or Start QT with /"taucoin-qt/" wallet if you have a desktop environment"
+echo "Now you can start the deamon with taucoind to work with taucoin-cli"
+echo "or Start QT with taucoin-qt wallet if you have a desktop environment"
